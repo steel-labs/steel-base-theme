1d5ab6aa9029c6c59ffafd9240507be5f52490a1
Giovanni Derks <giovanni.derks@steellondon.com>
2017-03-20 12:49:37 +0000

    Use webpack 2 instead of gulp
    - also use yarn instead of npm

--- default-theme/js/modules/cookie-bar.js
+++ default-theme/js/modules/cookie-bar.js
@@ -1,27 +1,28 @@
 /*jshint nonew:true, curly:true, noarg:true, forin:true, noempty:true, eqeqeq:true, strict:true, undef:true, bitwise:true, browser:true */
 
-var STEEL = STEEL || {};
-
-/* global Cookies */
-STEEL.cookieBar = (function ($) {
+class CookieBar
+{
     'use strict';
 
-    var cookie = Cookies.get('terms-accepted');
-    if(!cookie){
-        $.ajax({
-            method: 'POST',
-            url: '/home/cookies'
-        }).then(function(template){
-            $('body').prepend(template);
-            var $cookiebar = $('.cookie-bar');
+    constructor($) {
+        let cookie = Cookies.get('terms-accepted');
 
-            $cookiebar.find('.btn').bind('click', function(e){
-                e.preventDefault();
-                Cookies.set('terms-accepted', true, {expires: 365});
-                $cookiebar.remove();
-            })
+        if(!cookie){
+            $.ajax({
+                method: 'POST',
+                url: './default-theme/includes/cookies-bar.html'
+            }).then(function(template){
+                $('body').prepend(template);
+                let $cookiebar = $('.cookie-bar');
 
-        });
+                $cookiebar.find('.btn').bind('click', function(e){
+                    e.preventDefault();
+                    Cookies.set('terms-accepted', true, {expires: 365});
+                    $cookiebar.remove();
+                })
+            });
+        }
     }
+}
 
-})(jQuery);
+export default CookieBar;
